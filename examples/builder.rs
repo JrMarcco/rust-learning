fc34da7bebfc8c1ba1f4b73c842c20610204f843
jrmarcco <jrmarcco@gmail.com>
2024-09-11 17:21:58 +0800

    feat: derive_builder example.

---
+++ examples/builder.rs
@@ -0,0 +1,55 @@
+use anyhow::Result;
+use derive_builder::Builder;
+
+#[allow(dead_code)]
+#[derive(Debug, Builder)]
+struct Lorem {
+    ipsum: u32,
+}
+
+#[derive(Debug, Builder)]
+#[builder(try_setter, setter(into))]
+struct LoremN {
+    name: String,
+    ipsum: u8,
+}
+
+#[derive(Debug, Builder)]
+struct Ipsum {
+    #[builder(default = "self.default_name()")]
+    name: String,
+    #[builder(try_setter, setter(into, name = "foo"))]
+    dolor: u8,
+}
+
+impl IpsumBuilder {
+    fn default_name(&self) -> String {
+        String::from("jrmarcco")
+    }
+}
+
+fn main() -> Result<()> {
+    let lorem = LoremBuilder::default().ipsum(32).build()?;
+    println!("{:?}", lorem);
+
+    let mut lorem_builder = LoremBuilder::default();
+    lorem_builder.ipsum(42);
+
+    let lorem = lorem_builder.build()?;
+    println!("{:?}", lorem);
+
+    let lorem_n = LoremNBuilder::default()
+        .try_ipsum(1u16)?
+        .name("hello")
+        .build()
+        .expect("1 fits into a u8");
+    println!("{:?}, {} {}", lorem_n, lorem_n.name, lorem_n.ipsum);
+
+    let ipsum = IpsumBuilder::default()
+        .try_foo(1u16)?
+        .build()
+        .expect("1 fits into a u8");
+    println!("{:?}, {} {}", ipsum, ipsum.name, ipsum.dolor);
+
+    Ok(())
+}
